3dc208404afa21c1abe3376d2dbc64822b79845b
grubbykiller <xvy5021@gmail.com>
2021-01-28 22:10:05 -0800

    Update wrapper_functions.R

--- R/wrapper_functions.R
+++ R/wrapper_functions.R
@@ -1,27 +1,4 @@
-#
-# wrapper_beta = function(t){
-#   update_beta(t,nr_par,delta,delta2,leap,nc,omega,cbeta_prior,beta,tau_yes,tau_no,kappa,ymat)
-# }
-#
-# wrapper_yes = function(t){
-#   update_tau_yes(t,nc_par,delta_yes,delta2_yes,leap_tau,nr,beta,tau_yes,tau_no,kappa,ymat)
-# }
-#
-# wrapper_no = function(t){
-#   update_tau_no(t,nc_par,delta_no,delta2_no,leap_tau,nr,beta,tau_yes,tau_no,kappa,ymat)
-# }
-#
-# wrapper_kappa = function(t){
-#   update_kappa(t,nc_par,nr,beta,tau_yes,tau_no,kappa,ymat,kappa_a,ccc,t_sig)
-# }
-#
-# wrapper_waic = function(t){
-#   waic_cpp(t,nc_par,nr,beta,tau_yes,tau_no,kappa,ymat)
-# }
-#
-# wrapper_predict = function(t){
-#   predict(t,nc_par,nr,beta,tau_yes,tau_no,kappa,ymat)
-# }
+
 
 update_tsig = function(upper,lower,t_sig,check,nnn){
   l_s = which(check<=lower)
